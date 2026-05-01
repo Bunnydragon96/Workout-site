44040d4448f16ba26e3f205a7d16e144f72405d9
Bunnydragon96 <jcun0312@gmail.com>
2026-05-01 19:57:14 +0000

    Changes to app.py to use the youtube api to create the warmup exercise list.

--- build/app.py
+++ build/app.py
@@ -1,14 +1,45 @@
 ##Creating an exercise routine website using HTMl and Jinja blocks.
 ##Johnel Cunningham Rivera @1/29/2026
+##Johnel Cunningham River Modified@5/1/2026
 
+import os
+import requests
 from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
+PLAYLIST_ID = os.getenv("PLAYLIST_ID")
+
+def get_playlist_videos():
+    url = "https://www.googleapis.com/youtube/v3/playlistItems"
+    params = {
+        "part": "snippet",
+        "playlistId": PLAYLIST_ID,
+        "maxResults": 20,
+        "key": YOUTUBE_API_KEY
+    }
+    res = requests.get(url, params=params)
+    items = res.json().get("items", [])
+
+    videos = []
+    for item in items:
+        snippet = item["snippet"]
+        vid_id = snippet["resourceId"]["videoId"]
+        videos.append({
+            "title": snippet["title"],
+            "url": f"https://www.youtube.com/watch?v={vid_id}"
+        })
+    return videos
 
 ##List pages of easy, medium, full, and pushup triangle.
 @app.route("/")
 def start_page():
-    return render_template("warmup.html")
+    videos = get_playlist_videos()
+    return render_template("warmup.html", videos=videos)
 
 @app.route("/easy")
 def easy_workout():
